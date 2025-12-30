4b2175d3d27a02705c75be0890af18525dfb20b9
Manuel Alejandro Hernandez Marin <mhernandezm@inbursa.com>
2025-12-29 22:33:41 -0600

    [feat] - auth user id

--- GymApp/Features/Authentication/Services/AuthenticationState.swift
+++ GymApp/Features/Authentication/Services/AuthenticationState.swift
@@ -16,6 +16,7 @@ class AuthenticationService: ObservableObject {
     @Published var authenticationState_test: AuthenticationState = .loading
     
     private var authStateHandle: AuthStateDidChangeListenerHandle?
+    private var sessionManager = SessionDataManager.shared
     
     init() {
         configureAuthStateListener()
@@ -25,6 +26,15 @@ class AuthenticationService: ObservableObject {
         if let handle = authStateHandle {
             Auth.auth().removeStateDidChangeListener(handle)
         }
+    }
+    
+    
+    var currentUserId : String? {
+        if case .authenticated(let user) = authenticationState {
+            sessionManager.userId = user.uid
+            return user.uid
+        }
+        return nil
     }
     
     private func configureAuthStateListener() {
